ff29f8ecf1ea34b88f206ac9ea2b0e88493e340f
Voorman <ivar@voorman.eu>
2022-02-10 11:45:06 +0100

    Create bottum_5_film.sql
    added file

---
+++ bottum_5_film.sql
@@ -0,0 +1,14 @@
+-- Bottum 5 films
+SELECT
+	F.title,
+	SUM(P.amount) AS total_revenue
+FROM
+	payment P
+	INNER JOIN rental R ON P.rental_id = R.rental_id
+	INNER JOIN inventory I ON R.inventory_id = I.inventory_id
+	INNER JOIN film F ON I.film_id = F.film_id
+GROUP BY
+	F.title
+ORDER BY
+	total_revenue
+LIMIT 10;
